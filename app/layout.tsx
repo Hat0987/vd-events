4b8904cfa4423239620c586a6cebaeaf584f68a1
Hat0987 <111122395+Hat0987@users.noreply.github.com>
2024-01-05 20:08:31 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   title: 'VD Events',
   description: 'VD Events is a platform for event management.',
   icons: {
-    icon: '/assets/images/logo.svg'
+    icon: '/assets/images/logo.png'
   }
 }
 
