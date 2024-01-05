9792ff91a39ce95a77c3b2cdfee5128e21738ae2
Hat0987 <111122395+Hat0987@users.noreply.github.com>
2024-01-05 20:06:59 +0800

    Update MobileNav.tsx

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -28,8 +28,8 @@ const MobileNav = () => {
           <Image 
             src="/assets/images/logo.svg"
             alt="logo"
-            width={128}
-            height={38}
+            width={80}
+            height={80}
           />
           <Separator className="border border-gray-50" />
           <NavItems />
