63acb55604c381468f4ca7f43efdd910c400250a
Hat0987 <lepekolat@gmail.com>
2024-02-26 15:28:05 +0200

    Updates to UI

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -7,10 +7,10 @@ const Footer = () => {
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
         <Link href='/'>
           <Image 
-            src="/assets/images/logo.svg"
+            src="/assets/images/logo.png"
             alt="logo"
-            width={128}
-            height={38}
+            width={80}
+            height={80}
           />
         </Link>
 
